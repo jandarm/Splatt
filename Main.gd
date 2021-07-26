afee027ab53530a559fa510e43ee986970085105
Daniel <50790074+jandarm@users.noreply.github.com>
2021-07-26 15:16:37 +0300

    Initial commit

---
+++ Main.gd
@@ -0,0 +1,60 @@
+extends Node2D
+
+export (PackedScene) var Splatter
+
+var mouse_left_down = false
+var window_size
+var ev
+
+func _ready():
+	pass 
+
+# warning-ignore:unused_argument
+func _process(delta):
+	#rappid fire
+#	if mouse_left_down:
+#		if(ev.position < window_size/2):
+#			var new_splat = Splatter.instance()
+#			new_splat.position = get_viewport().get_mouse_position()
+#			new_splat.change_tint("yellow")
+#			add_child(new_splat)
+#		elif(ev.position >= window_size/2):
+#			var new_splat = Splatter.instance()
+#			new_splat.position = get_viewport().get_mouse_position()
+#			new_splat.change_tint("purple")
+#			add_child(new_splat)
+		
+	pass
+
+func _input(event):
+	# size of the viewport.
+	window_size = get_viewport_rect().size
+	#var scale = Vector2(get_viewport().size.x, get_viewport().size.y)
+	ev = event
+   # Mouse in viewport coordinates.
+	if(event.is_action_pressed("LClick")):
+		#print("Mouse Click: ", event.position)
+		if(ev.position < window_size/2):
+			var new_splat = Splatter.instance()
+			new_splat.position = get_viewport().get_mouse_position()
+			new_splat.change_tint("yellow")
+			add_child(new_splat)
+			#new_splat.scale = Vector2(10, 10)
+		elif(ev.position >= window_size/2):
+			var new_splat = Splatter.instance()
+			new_splat.position = get_viewport().get_mouse_position()
+			new_splat.change_tint("purple")
+			add_child(new_splat)
+	
+	#for rappid fire
+#	if event is InputEventMouseButton:
+#		if event.button_index == 1 and event.is_pressed():
+#			mouse_left_down = true
+#		elif event.button_index == 1 and not event.is_pressed():
+#			mouse_left_down = false
+	pass
+
+
+func _on_Area2D_body_entered(_body):
+	print("1")
+	pass # Replace with function body.
---
+++ Splatter.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+onready var motion = self.applied_force
+
+func _ready():
+	pass
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+	pass
+
+func change_tint(color):
+	#for rappid fire
+#	randomize()
+#	get_node("Sprite").material.set("shader_param/silhouette_color", Color(randf(), randf(), randf(), 1))
+	match color:
+		"yellow": get_node("Sprite").material.set("shader_param/silhouette_color", Color("#fcba03"))
+		"purple": get_node("Sprite").material.set("shader_param/silhouette_color", Color("#890ce8"))
+		_: get_node("Sprite").material.set("shader_param/silhouette_color", Color(0, 0, 0, 1))
+	pass
